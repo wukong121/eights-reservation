1a2a97eb7effb9d353656999aca0291c9cdd01aa
peter <peterwangyixing@gmail.com>
2023-10-13 23:55:28 +0800

    Add StudentService interface.

---
+++ src/main/java/com/bupt/eights/service/StudentService.java
@@ -0,0 +1,20 @@
+package com.bupt.eights.service;
+
+import com.bupt.eights.model.Appointment;
+import com.bupt.eights.model.Student;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+public interface StudentService {
+    
+    void saveStudent(Student student);
+    
+    Student findStudentByPhoneNumber(Long phoneNumber);
+    
+    Map<Date, List<Appointment>> getUpcomingAppointment(List<Appointment> appointmentList);
+    
+    Map<Date, List<Appointment>> getOverdueAppointment(List<Appointment> appointmentList);
+    
+}
