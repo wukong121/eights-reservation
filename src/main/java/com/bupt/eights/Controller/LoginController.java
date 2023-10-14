5faf9d3f25e8fbddacce862d742396becb01c5e5
peter <peterwangyixing@gmail.com>
2023-10-14 21:20:39 +0800

    Add HomeController and LoginController.

--- src/main/java/com/bupt/eights/Controller/LoginController.java
+++ src/main/java/com/bupt/eights/Controller/LoginController.java
@@ -1,5 +1,49 @@
 package com.bupt.eights.Controller;
 
-public class LoginController {
 
+import com.bupt.eights.model.AuthorityRole;
+import com.bupt.eights.model.User;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+    
+    private String redirectByRole(HttpServletRequest request) {
+        if (request.isUserInRole(AuthorityRole.ROLE_ADMIN.toString())) {
+            return "redirect:/admin";
+        }
+        if (request.isUserInRole(AuthorityRole.ROLE_STUDENT.toString())) {
+            return "redirect:/students/home";
+        }
+        return "";
+    }
+    
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String showLogin(@ModelAttribute(value = "user") User user, HttpServletRequest request) {
+        String path = redirectByRole(request);
+        if (path.isEmpty()) {
+            return "login";
+        }
+        return path;
+    }
+    
+    @RequestMapping(value = "/login-success", method = RequestMethod.GET)
+    public String loginSuccess(HttpServletRequest request) {
+        String path = redirectByRole(request);
+        if (path.isEmpty()) {
+            return "redirect:/home";
+        }
+        return path;
+    }
+    
+    @RequestMapping(value = "/login-failed", method = RequestMethod.GET)
+    public String loginFailed(@ModelAttribute(value = "user") User user, Model model) {
+        model.addAttribute("fail", true);
+        return "login";
+    }
 }
