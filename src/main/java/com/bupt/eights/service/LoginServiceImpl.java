35fbb804503839e1e57aa907cb06b3dcc27f54b8
Vercell <747024049@qq.com>
2023-10-27 16:37:00 +0800

    Add encrypt password

--- src/main/java/com/bupt/eights/service/LoginServiceImpl.java
+++ src/main/java/com/bupt/eights/service/LoginServiceImpl.java
@@ -6,6 +6,7 @@ import com.bupt.eights.model.AuthorityRole;
 import com.bupt.eights.model.User;
 import com.bupt.eights.utils.ServiceUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
@@ -29,6 +30,10 @@ public class LoginServiceImpl implements LoginService {
         Date now = new Date();
         SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         user.setCreateTime(date.format(now));
+        // encrypt password
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        String encode = bCryptPasswordEncoder.encode(user.getPassword());
+        user.setPassword(encode);
         
         return userMapper.insertUser(user);
     }
